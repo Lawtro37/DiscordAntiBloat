412955a238b4aa328ab1383e20d6868f5ace04c0
Lawtro <98205608+Lawtro37@users.noreply.github.com>
2025-07-12 21:43:17 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,66 +1,23 @@
 import settings from "./settings.jsx";
 import { storage } from "@vendetta/plugin";
-import { findByProps } from "@vendetta/metro";
-import { after } from "@vendetta/patcher";
-
-let patches = [];
-
-function safePatch(name, fn) {
-  try {
-    const unpatch = fn();
-    if (unpatch) patches.push(unpatch);
-  } catch (e) {
-    console.error(`[AntiBloat] Error in ${name}:`, e);
-  }
-}
 
 export default {
   settings,
 
   onLoad() {
-    // Set default settings
-    storage.hideNitro ??= true;
-    storage.hideQuests ??= true;
-    storage.hideTabs ??= true;
+    console.log("[MyFirstPlugin] Loaded!");
 
-    // Nitro upsells
-    if (storage.hideNitro) {
-      safePatch("hideNitroUpsell", () => {
-        const Upsell = findByProps("tryItOutCtaText");
-        if (!Upsell) return;
-        return after("default", Upsell, () => null);
-      });
-    }
+    // Set a default value
+    storage.enabled ??= true;
 
-    // Server Quests
-    if (storage.hideQuests) {
-      safePatch("hideQuests", () => {
-        const GuildHome = findByProps("useGuildHome");
-        if (!GuildHome) return;
-        return after("useGuildHome", GuildHome, (_, ret) => {
-          ret.shouldShowQuests = false;
-          ret.guildHomeSections = ret.guildHomeSections?.filter(
-            s => s.sectionType !== "QUESTS"
-          );
-        });
-      });
-    }
-
-    // Explore and Monetization tabs
-    if (storage.hideTabs) {
-      safePatch("hideExploreAndMonetization", () => {
-        const TabBar = findByProps("TabBarItem");
-        if (!TabBar) return;
-        return after("TabBarItem", TabBar, ([args], ret) => {
-          if (["Explore", "Monetization"].includes(args.title)) return null;
-          return ret;
-        });
-      });
+    if (storage.enabled) {
+      console.log("[MyFirstPlugin] Setting is enabled!");
+    } else {
+      console.log("[MyFirstPlugin] Setting is disabled!");
     }
   },
 
   onUnload() {
-    patches.forEach(unpatch => unpatch());
-    patches = [];
-  },
+    console.log("[MyFirstPlugin] Unloaded!");
+  }
 };
